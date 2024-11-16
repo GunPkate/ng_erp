cd1de66497da2ec600f01cab70c067f7ec6079d7
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-11-16 09:24:04 +0700

    start project and create mat-menu component

---
+++ src/components/button-menu/button-menu.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu'; 
+
+@Component({
+  selector: 'app-button-menu',
+  templateUrl: './button-menu.component.html',
+  standalone: true,
+  imports: [MatMenuModule],
+  styleUrls: ['./button-menu.component.css']
+})
+export class ButtonMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
