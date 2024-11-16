08d8459b19ad4cfb4d9cd97af61f4de0e39bb36c
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-11-16 14:24:03 +0700

    add btn submenu and event trigger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng_erp';
+  topic = ''
+  inputBtn1 = [
+    {
+      btnTitleName: "Sale",
+      subMenu: "btn1",
+      subBtn: [
+        {btnTitleName: "Invoice"},
+        {btnTitleName: "Receipt"},
+        {btnTitleName: "Report"},
+      ]
+    },
+    
+  ]
+
+  triggerBtnEvent(value: any){
+    this.topic = value
+    console.log(value)
+  }
 }
--- src/components/button-menu/button-menu.component.ts
+++ src/components/button-menu/button-menu.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu'; 
 
 @Component({
   selector: 'app-button-menu',
   templateUrl: './button-menu.component.html',
   standalone: true,
-  imports: [MatMenuModule],
+  imports: [MatMenuModule,CommonModule],
   styleUrls: ['./button-menu.component.css']
 })
 export class ButtonMenuComponent implements OnInit {
 
   constructor() { }
 
+  @Input() btnTitle?: string;
+  @Input() btnMenuList?: any;
+  @Output('BtnEvent') 
+  btnEvent:EventEmitter<string> = new EventEmitter<string>;
+
+  subBtn: any
   ngOnInit(): void {
+    if(this.btnMenuList.subBtn){
+      this.subBtn = this.btnMenuList.subBtn
+    }
   }
 
+  triggerBtn(value: string){
+    this.btnEvent.emit(value);
+    console.log(value)
+  }
 }
