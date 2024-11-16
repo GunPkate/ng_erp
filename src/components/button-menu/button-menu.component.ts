75a46c67d490c8ca35426e80a0d5295fa4c5f3a2
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-11-16 09:43:28 +0700

    install bootstrap

--- src/components/button-menu/button-menu.component.ts
+++ src/components/button-menu/button-menu.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {MatMenuModule} from '@angular/material/menu'; 
+// import {MatMenuModule} from '@angular/material/menu'; 
 
 @Component({
   selector: 'app-button-menu',
   templateUrl: './button-menu.component.html',
   standalone: true,
-  imports: [MatMenuModule],
+  // imports: [MatMenuModule],
   styleUrls: ['./button-menu.component.css']
 })
 export class ButtonMenuComponent implements OnInit {
