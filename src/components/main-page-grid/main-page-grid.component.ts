79695863e46d28dd32865f4ab396c55fd78e52ee
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-11-17 17:17:35 +0700

    navigate main page by url

--- src/components/main-page-grid/main-page-grid.component.ts
+++ src/components/main-page-grid/main-page-grid.component.ts
@@ -1,6 +1,7 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { Router } from '@angular/router';
 import { Btn } from 'src/interface/Btn';
 import { InputBtn } from 'src/interface/InputBtn';
 
@@ -8,21 +9,26 @@ import { InputBtn } from 'src/interface/InputBtn';
 @Component({
   selector: 'app-main-page-grid',
   standalone: true,
-  imports: [MatGridListModule, NgFor],
+  imports: [MatGridListModule, NgFor, NgIf],
   templateUrl: './main-page-grid.component.html',
   styleUrls: ['./main-page-grid.component.css']
 })
 export class MainPageGridComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
   @Input() btnImport!: Btn[]
-  subBtnGrid: InputBtn[] | undefined
+  subBtnGrid: InputBtn[] = []
   ngOnInit(): void {
     // subBtnGrid = this.btnImport.forEach(element => {
-      this.btnImport.forEach( x=> 
-        this.subBtnGrid = x.inputBtn
+    let checkUrl = this.route.routerState.snapshot.url.replace("/","")  
+    this.btnImport.forEach( x=> {
+        if(checkUrl === x.title.toLowerCase()){
+          this.subBtnGrid = x.inputBtn
+        } 
+      }
       )
       console.log("this.subBtnGrid",this.subBtnGrid)
+      console.log("this route",)
     // });
   }
 
