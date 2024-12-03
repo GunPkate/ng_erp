3a4ef5f90a380859120b1819b16670a79dce364b
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-12-03 19:01:50 +0700

    [Refactor] suplier invoice page

--- src/pages/p03-purchase/p03-purchase.module.ts
+++ src/pages/p03-purchase/p03-purchase.module.ts
@@ -4,13 +4,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { P03PurchaseComponent } from './p03-purchase.component';
 import { Form03SupplierComponent } from 'src/form/p03-purchase/form-lv1Supplier/form03-supplier.component';
-import { Form03Lv2Component } from 'src/form/p03-purchase/form-lv2/form03-lv2.component';
+import { Form03SupplierinvoiceComponent } from 'src/form/p03-purchase/form-lv2Supplierinvoice/form03-supplierinvoice.component';
 import { Form03Lv3Component } from 'src/form/p03-purchase/form-lv3/form03-lv3.component';
 import { Form03lv4Component } from 'src/form/p03-purchase/form-lv4/form03-lv4.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SupplierService } from 'src/shared/services/S03Purchase/S03_Supplier';
 import { SupplierBehaviorSubj } from 'src/shared/behaviorsubject/Supplier';
 import { HttpClientModule } from '@angular/common/http';
+import { SupplierInvoiceBehaviorSubj } from 'src/shared/behaviorsubject/SupplierInvoice';
 
 export const p03routes:Routes = [
   {
@@ -18,7 +19,7 @@ export const p03routes:Routes = [
     component: P03PurchaseComponent,
     children:[
       {path: 'supplier', component: Form03SupplierComponent},
-      {path: 'purchaseinvoice', component: Form03Lv2Component},
+      {path: 'purchaseinvoice', component: Form03SupplierinvoiceComponent},
       {path: 'purchasepayment', component: Form03Lv3Component},
       {path: 'allpurchase', component: Form03lv4Component},
     ]
@@ -33,7 +34,7 @@ export const p03routes:Routes = [
     HttpClientModule,
     ButtonMenuComponent,
     Form03SupplierComponent,
-    Form03Lv2Component,
+    Form03SupplierinvoiceComponent,
     Form03Lv3Component,
     Form03lv4Component,
     RouterModule.forChild(p03routes),
@@ -42,7 +43,7 @@ export const p03routes:Routes = [
     MatInputModule
   ],
   providers: [
-    SupplierService, SupplierBehaviorSubj
+    SupplierService, SupplierBehaviorSubj, SupplierInvoiceBehaviorSubj
   ],
 
 })
--- src/shared/services/S03Purchase/S03_Supplier.ts
+++ src/shared/services/S03Purchase/S03_Supplier.ts
@@ -1,17 +1,25 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { SupplierBehaviorSubj } from "src/shared/behaviorsubject/Supplier";
+import { SupplierInvoiceBehaviorSubj } from "src/shared/behaviorsubject/SupplierInvoice";
 
 @Injectable()
 export class SupplierService{
     constructor(
         private http: HttpClient,
-        private supplierBehaviorSubj: SupplierBehaviorSubj
+        private supplierBehaviorSubj: SupplierBehaviorSubj,
+        private supplierInvoiceBehaviorSubj: SupplierInvoiceBehaviorSubj
     ){}
 
     loadSupplier(){
         this.http.get('http://localhost:3000/supplier/all').subscribe( (res:any)=>{
             this.supplierBehaviorSubj.setSupplierList(res)
+        })
+    }
+
+    loadSupplierInvoice(){
+        this.http.get('http://localhost:3000/supplier/all').subscribe( (res:any)=>{
+            this.supplierInvoiceBehaviorSubj.setSupplierInvoiceList(res)
         })
     }
 }
