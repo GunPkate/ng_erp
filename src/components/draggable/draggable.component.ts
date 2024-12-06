be641891d5cca3815713ad0c52ac46b310710be9
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-12-06 09:09:41 +0700

    [Set] Page Business flow

--- src/components/draggable/draggable.component.ts
+++ src/components/draggable/draggable.component.ts
@@ -1,17 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 import {DragDropModule} from '@angular/cdk/drag-drop'; 
+import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-draggable',
   standalone: true,
-  imports: [DragDropModule],
+  imports: [DragDropModule, CdkAccordionModule, NgFor],
   templateUrl: './draggable.component.html',
   styleUrls: ['./draggable.component.css']
 })
 export class Draggable implements OnInit {
 
   constructor() { }
-
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  expandedIndex = 0;
   ngOnInit(): void {
 
   }
