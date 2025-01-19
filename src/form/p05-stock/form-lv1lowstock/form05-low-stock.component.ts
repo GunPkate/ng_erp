0f47e50be04d2c325608d50f0592d9865970df7b
DESKTOP-61UD6OJ\GunP <gundash1@gmail.com>
2025-01-19 18:05:30 +0700

    [Feature] add low stock line chart

--- src/form/p05-stock/form-lv1lowstock/form05-low-stock.component.ts
+++ src/form/p05-stock/form-lv1lowstock/form05-low-stock.component.ts
@@ -22,18 +22,35 @@ export class Form05LowStockComponent implements OnInit {
 
   title05 = 'Product'
 
-  data = [
+  dataSource = [
     {
       id: 'chartOne',
       title: 'Chart One',
-      type: 'bar',
+      type: 'line',
       lables: ['Villas By Taru', 'Watch Tower'],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 2,
       data: [18, 12],
     },
     {
       id: 'chartTwo',
       title: 'Chart Two',
-      type: 'bar',
+      type: 'line',
       lables: ['Tea Avenue', 'The Grill Bar'],
       data: [18, 12, 2, 7],
     },
@@ -41,20 +58,20 @@ export class Form05LowStockComponent implements OnInit {
 
   ngOnInit(){}
   ngAfterViewInit() {
-    this.data.forEach((element) => {
+    this.dataSource.forEach((element) => {
       this.generateType(element);
     });
   }
 
-  generateType(data: any) {
-    const myChart = new Chart(data.id, {
-      type: 'bar',
+  generateType(dataSet: any) {
+    const myChart = new Chart(dataSet.id, {
+      type: 'line',
       data: {
-        labels: data.labels,
+        labels: dataSet.labels,
         datasets: [
           {
             label: '# of Votes',
-            data: data.data,
+            data: dataSet.data,
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
@@ -71,7 +88,7 @@ export class Form05LowStockComponent implements OnInit {
               'rgba(153, 102, 255, 1)',
               'rgba(255, 159, 64, 1)',
             ],
-            borderWidth: 1,
+            borderWidth: 2,
           },
         ],
       },
